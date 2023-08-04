e0bfb62e4abd8fa632c4861242b76524adfb3bc3
unimafy <nima.yonten1729@gmail.com>
2023-08-04 20:07:49 +0600

    initial commit

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var messageDiv = document.getElementById('message');
+  messageDiv.textContent = 'Hello, Chrome Extension World!';
+});
+
